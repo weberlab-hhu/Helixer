e5de0647cdf1bbb954d46626490b377eea7d11be
Felix Stiehler <felix.stiehler87@gmail.com>
2019-05-10 16:02:41 +0200

    all tests fixed but also many tests taken out

--- helixerprep/tests/test_helixer_prep.py
+++ helixerprep/tests/test_helixer_prep.py
@@ -74,13 +74,16 @@ def setup_dummyloci_for_numerify(simplify=False):
     coordinate = sess.query(geenuff.orm.Coordinate).first()
     coord_handler = CoordinateHandler(coordinate)
 
-    # remove erroneous transcripts and their childran
     if simplify:
         for t in ['x', 'z']:
-            transcript = sess.query(Transcribed).\
-                             filter(Transcribed.given_name == t).first()
-            rm_transcript_and_children(transcript, sess)
-
+            transcript = sess.query(Transcribed).filter(Transcribed.given_name == t).first()
+            # remove transcript and its children
+            for piece in transcript.transcribed_pieces:
+                for feature in piece.features:
+                    sess.delete(feature)
+                sess.delete(piece)
+            sess.delete(transcript)
+        sess.commit()
     return sess, controller, coord_handler
 
 
@@ -283,6 +286,22 @@ def test_numerify_from_gr0():
 
 
 def test_sequence_slicing():
+    _, coord_handler = memory_import_fasta('testdata/dummyloci.fa')
+    seq_numerifier = SequenceNumerifier(coord_handler=coord_handler,
+                                        is_plus_strand=True,
+                                        max_len=50)
+    num_list = seq_numerifier.coord_to_matrices()
+    print([x.shape for x in num_list])
+    # [(50, 4), (50, 4), (50, 4), (50, 4), (50, 4), (50, 4), (50, 4), (27, 4), (28, 4)]
+    assert len(num_list) == 9
+
+    for i in range(7):
+        assert np.array_equal(num_list[i], np.full([50, 4], 0.25, dtype=np.float32))
+    for i in [7, 8]:  # for the last two, just care that they're about the expected size...
+        assert np.array_equal(num_list[i][:27], np.full([27, 4], 0.25, dtype=np.float32))
+
+
+def test_coherent_slicing():
     """Tests for coherent output when slicing the 405 bp long dummyloci.
     The correct divisions are already tested in the Stepper test.
     The array format of the individual matrices are tested in
@@ -334,6 +353,19 @@ def test_minus_strand_numerify():
     expect = np.flip(expect, axis=0)
     assert np.array_equal(nums, expect)
 
+    # minus strand and actual cutting
+    numerifier = BasePairAnnotationNumerifier(coord_handler=coord_handler,
+                                              features=coord_handler.data.features,
+                                              is_plus_strand=False,
+                                              max_len=50)
+    num_list = numerifier.coord_to_matrices()
+
+    expect = np.zeros([100, 3], dtype=np.float32)
+    expect[10:41, 0] = 1.
+
+    assert np.array_equal(num_list[0], np.flip(expect[50:100], axis=0))
+    assert np.array_equal(num_list[1], np.flip(expect[0:50], axis=0))
+
     # sequences on minus strand
     _, coord_handler = memory_import_fasta('testdata/biointerp_loci.fa')
     numerifier = SequenceNumerifier(coord_handler=coord_handler,
@@ -375,271 +407,3 @@ def test_h5_gen():
     assert np.array_equal(labels[1], expect[202:])
 
 
-###############################
-###############################
-###############################
-
-# rm test and replace with feature by coordinate test for interval
-def test_intervaltree():
-    controller = construct_slice_controller()
-    controller.fill_intervaltrees()
-    print(controller.interval_trees.keys())
-    for intv in controller.interval_trees["1"]:
-        s = '{}:{}, {}'.format(intv.begin, intv.end, intv.data.data)
-        print(s)
-    # check that one known area has two errors, and one transcription termination site as expected
-    intervals = controller.interval_trees['1'][399:406]
-    assert len(intervals) == 3
-    print(intervals, '...intervals')
-    print([x.data.data.type.value for x in intervals])
-    errors = [x for x in intervals if x.data.data.type.value == geenuff.types.ERROR]
-
-    assert len(errors) == 2
-    transcribeds = [x for x in intervals if x.data.data.type.value == geenuff.types.TRANSCRIBED]
-
-    assert len(transcribeds) == 1
-    # check that the major filter functions work
-    sls = controller.get_super_loci_frm_slice(seqid='1', start=300, end=405, is_plus_strand=True)
-    assert len(sls) == 1
-    assert isinstance(list(sls)[0], slicer.SuperLocusHandler)
-
-    features = controller.get_features_from_slice(seqid='1', start=0, end=1, is_plus_strand=True)
-    assert len(features) == 3
-    starts = [x for x in features if x.data.type.value == geenuff.types.TRANSCRIBED]
-
-    assert len(starts) == 2
-    errors = [x for x in features if x.data.type.value == geenuff.types.ERROR]
-    assert len(errors) == 1
-
-
-class TransspliceDemoDataSlice(TransspliceDemoData):
-    def __init__(self, sess, engine):
-        super().__init__(sess)
-        self.core_queue = slicer.CoreQueue(session=sess, engine=engine)
-        self.genome = geenuff.orm.Genome()
-        self.old_coor = geenuff.orm.Coordinate(genome=self.genome, seqid='a', start=1, end=2000)
-        self.slh = slicer.SuperLocusHandler(self.sl)
-        self.scribedh = slicer.TranscribedHandler(self.scribed)
-        self.scribedfliph = slicer.TranscribedHandler(self.scribedflip)
-
-        self.ti = slicer.TranscriptTrimmer(transcript=self.scribedh,
-                                           super_locus=self.slh,
-                                           sess=sess,
-                                           core_queue=self.core_queue)
-        self.tiflip = slicer.TranscriptTrimmer(transcript=self.scribedfliph,
-                                               super_locus=self.slh,
-                                               sess=sess,
-                                               core_queue=self.core_queue)
-
-
-class SimplestDemoData(object):
-    def __init__(self, sess, engine, genome=None):
-        self.core_queue = slicer.CoreQueue(session=sess, engine=engine)
-        if genome is None:
-            self.genome = geenuff.orm.Genome()
-        else:
-            self.genome = genome
-        self.old_coor = geenuff.orm.Coordinate(seqid='a', start=0, end=1000, genome=self.genome)
-        # setup 1 transition (encoding e.g. a transcript split across a miss assembled scaffold...):
-        # 2) scribedlong - [[CD<-],[->AB]] -> ABCD, -> two transcribed pieces: one forward, one backward
-        self.sl, self.slh = setup_data_handler(slicer.SuperLocusHandler, geenuff.orm.SuperLocus)
-        self.scribedlong, self.scribedlongh = setup_data_handler(slicer.TranscribedHandler,
-                                                                 geenuff.orm.Transcribed,
-                                                                 super_locus=self.sl)
-
-        self.tilong = slicer.TranscriptTrimmer(transcript=self.scribedlongh,
-                                               super_locus=self.slh,
-                                               sess=sess,
-                                               core_queue=self.core_queue)
-
-        self.pieceAB = geenuff.orm.TranscribedPiece(position=0)
-        self.pieceCD = geenuff.orm.TranscribedPiece(position=1)
-        self.scribedlong.transcribed_pieces = [self.pieceAB, self.pieceCD]
-
-        self.fAB = geenuff.orm.Feature(transcribed_pieces=[self.pieceAB],
-                                       coordinate=self.old_coor,
-                                       start=190,
-                                       end=210,
-                                       start_is_biological_start=True,
-                                       end_is_biological_end=False,
-                                       given_name='AB',
-                                       is_plus_strand=True,
-                                       type=geenuff.types.TRANSCRIBED)
-
-        self.fCD = geenuff.orm.Feature(transcribed_pieces=[self.pieceCD],
-                                       coordinate=self.old_coor,
-                                       is_plus_strand=False,
-                                       start=110,
-                                       end=90,
-                                       start_is_biological_start=False,
-                                       end_is_biological_end=True,
-                                       type=geenuff.types.TRANSCRIBED,
-                                       given_name='CD')
-
-        self.pieceAB.features.append(self.fAB)
-        self.pieceCD.features.append(self.fCD)
-
-        sess.add_all([self.scribedlong, self.pieceAB, self.pieceCD, self.fAB, self.fCD,
-                      self.old_coor, self.sl])
-        sess.commit()
-        self.slh.make_all_handlers()
-
-
-#redo for numerify
-def test_transition_unused_coordinates_detection():
-    sess, engine = mk_memory_session()
-    d = SimplestDemoData(sess, engine)
-    # modify to coordinates with complete contain, should work fine
-    genome = d.genome
-    new_coords = geenuff.orm.Coordinate(seqid='a', start=0, end=300, genome=genome)
-    sess.add(new_coords)
-    sess.commit()
-    d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=True)
-    d.core_queue.execute_so_far()
-    d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=False)
-    d.core_queue.execute_so_far()
-    assert d.pieceCD in d.scribedlong.transcribed_pieces  # should now keep original at start
-    assert d.pieceAB in d.scribedlong.transcribed_pieces
-    # modify to coordinates across tiny slice, include those w/o original features, should work fine
-    d = SimplestDemoData(sess, engine)
-    new_coords_list = [geenuff.orm.Coordinate(genome=genome, seqid='a', start=185, end=195),
-                       geenuff.orm.Coordinate(genome=genome, seqid='a', start=195, end=205),
-                       geenuff.orm.Coordinate(genome=genome, seqid='a', start=205, end=215)]
-
-    for new_coords in new_coords_list:
-        sess.add(new_coords)
-        sess.commit()
-        print('fw {}, {}'.format(new_coords.id, new_coords))
-        d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=True)
-        d.core_queue.execute_so_far()
-        print([x.id for x in d.tilong.transcript.data.transcribed_pieces])
-
-    new_coords_list = [geenuff.orm.Coordinate(genome=genome, seqid='a', start=105, end=115),
-                       geenuff.orm.Coordinate(genome=genome, seqid='a', start=95, end=105),
-                       geenuff.orm.Coordinate(genome=genome, seqid='a', start=85, end=95)]
-    for new_coords in new_coords_list:
-        sess.add(new_coords)
-        sess.commit()
-        print('\nstart mod for coords, - strand', new_coords)
-        d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=False)
-        d.core_queue.execute_so_far()
-
-    assert d.pieceCD in d.scribedlong.transcribed_pieces
-    assert d.pieceAB in d.scribedlong.transcribed_pieces
-
-    # try and slice before coordinates, should raise error
-    d = SimplestDemoData(sess, engine)
-    new_coords = geenuff.orm.Coordinate(genome=genome, seqid='a', start=0, end=10)
-    sess.add(new_coords)
-    sess.commit()
-    with pytest.raises(slicer.NoFeaturesInSliceError):
-        d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=True)
-    # try and slice after coordinates, should raise error
-    d = SimplestDemoData(sess, engine)
-    new_coords = geenuff.orm.Coordinate(genome=genome, seqid='a', start=399, end=410)
-    sess.add(new_coords)
-    sess.commit()
-    with pytest.raises(slicer.NoFeaturesInSliceError):
-        d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=True)
-    # try and slice between slices where there are no coordinates, should raise error
-    d = SimplestDemoData(sess, engine)
-    new_coords = geenuff.orm.Coordinate(genome=genome, seqid='a', start=149, end=160)
-    sess.add(new_coords)
-    sess.commit()
-    with pytest.raises(slicer.NoFeaturesInSliceError):
-        d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=True)
-    d = SimplestDemoData(sess, engine)
-    with pytest.raises(slicer.NoFeaturesInSliceError):
-        d.tilong.modify4new_slice(new_coords=new_coords, is_plus_strand=False)
-
-# redo for numerify
-def test_slicing_featureless_slice_inside_locus():
-    controller = construct_slice_controller()
-    controller.fill_intervaltrees()
-    genome = controller.get_one_genome()
-    slh = controller.super_loci[0]
-    transcript = [x for x in slh.data.transcribeds if x.given_name == 'y'][0]
-    slices = (('1', 'A' * 40, 0, 40, 'train'),
-              ('1', 'A' * 40, 40, 80, 'train'),
-              ('1', 'A' * 40, 80, 120, 'train'))
-    slices = iter(slices)
-    controller._slice_annotations_1way(slices, genome=genome, is_plus_strand=True)
-
-    for piece in transcript.transcribed_pieces:
-        print('got piece: {}\n-----------\n'.format(piece))
-        for feature in piece.features:
-            print('    {}'.format(feature))
-    coordinate40 = controller.session.query(geenuff.orm.Coordinate).filter(
-        geenuff.orm.Coordinate.start == 40
-    ).first()
-    features40 = coordinate40.features
-    print(features40)
-
-    # x & y -> 2 translated, 2 transcribed each, z -> 2 error
-    assert len([x for x in features40 if x.type.value == geenuff.types.CODING]) == 2
-    assert len([x for x in features40 if x.type.value == geenuff.types.TRANSCRIBED]) == 2
-    assert len(features40) == 5
-    assert set([x.type.value for x in features40]) == {
-        geenuff.types.CODING,
-        geenuff.types.TRANSCRIBED,
-        geenuff.types.ERROR
-    }
-
-
-def rm_transcript_and_children(transcript, sess):
-    for piece in transcript.transcribed_pieces:
-        for feature in piece.features:
-            sess.delete(feature)
-        sess.delete(piece)
-    sess.delete(transcript)
-    sess.commit()
-
-
-# change to test edge case numerification (or do that in another test)
-def test_reslice_at_same_spot():
-    controller = construct_slice_controller()
-    slh = controller.super_loci[0]
-    # simplify
-    transcripty = [x for x in slh.data.transcribeds if x.given_name == 'y'][0]
-    transcriptz = [x for x in slh.data.transcribeds if x.given_name == 'z'][0]
-    rm_transcript_and_children(transcripty, controller.session)
-    rm_transcript_and_children(transcriptz, controller.session)
-    # slice
-    controller.fill_intervaltrees()
-    print('controller.sess', controller.session)
-    slices = (('1', 'A' * 100, 0, 100, 'train'), )
-    controller._slice_annotations_1way(iter(slices), controller.get_one_genome(), is_plus_strand=True)
-    controller.session.commit()
-    old_len = len(controller.session.query(geenuff.orm.TranscribedPiece).all())
-    print('used to be {} linkages'.format(old_len))
-    controller._slice_annotations_1way(iter(slices), controller.get_one_genome(), is_plus_strand=True)
-    controller.session.commit()
-    assert old_len == len(controller.session.query(geenuff.orm.TranscribedPiece).all())
-
-
-# fix slice to matrix
-def test_live_slicing():
-    sess, coord_handler = setup_simpler_numerifier()
-
-    # base pair annotations on minus strand with slicing
-    numerifier = numerify.BasePairAnnotationNumerifier(coord_handler=coord_handler,
-                                                       is_plus_strand=False)
-    num_list = list(numerifier.slice_to_matrices(max_len=50))
-
-    expect = np.zeros([100, 3], dtype=np.float32)
-    expect[10:41, 0] = 1.
-
-    assert np.array_equal(num_list[0], np.flip(expect[50:100], axis=0))
-    assert np.array_equal(num_list[1], np.flip(expect[0:50], axis=0))
-
-    # dummyloci sequences slicing
-    _, coord_handler = memory_import_fasta('testdata/dummyloci.fa')
-    numerifier = numerify.SequenceNumerifier(coord_handler=coord_handler, is_plus_strand=True)
-    num_list = list(numerifier.slice_to_matrices(max_len=50))
-    print([x.shape for x in num_list])
-    # [(50, 4), (50, 4), (50, 4), (50, 4), (50, 4), (50, 4), (50, 4), (27, 4), (28, 4)]
-    assert len(num_list) == 9
-    for i in range(7):
-        assert np.array_equal(num_list[i], np.full([50, 4], 0.25, dtype=np.float32))
-    for i in [7, 8]:  # for the last two, just care that they're about the expected size...
-        assert np.array_equal(num_list[i][:27], np.full([27, 4], 0.25, dtype=np.float32))
