caa1548882fd19baeda818a7e7e87bee63fd8b48
Felix Stiehler <felix.stiehler87@gmail.com>
2019-06-11 14:01:16 +0200

    fixed file chunking

--- helixerprep/export/exporter.py
+++ helixerprep/export/exporter.py
@@ -1,4 +1,5 @@
 import os
+import h5py
 import numpy as np
 import deepdish as dd
 import sklearn
@@ -18,7 +19,7 @@ class ExportController(object):
         if not os.path.isdir(self.out_dir):
             os.mkdir(self.out_dir)
         elif os.listdir(self.out_dir):
-            print('WARNING: target dir {} not empty'.format(self.out_dir))
+            print('WARNING: target dir {} is not empty'.format(self.out_dir))
         self._mk_session()
 
     def _mk_session(self):
@@ -43,20 +44,23 @@ class ExportController(object):
             }
             return d
 
-        def save_data(data, file_chunk_count, shuffle):
+        def get_file_name(i):
+            return os.path.join(self.out_dir, 'data' + str(i) + '.h5')
+
+        def save_data(data, i, shuffle):
             if shuffle:
                 x, y, m = data['inputs'], data['labels'], data['label_masks']
                 x, y, m = sklearn.utils.shuffle(x, y, m)
                 data['inputs'], data['labels'], data['label_masks'] = x, y, m
-            dd.io.save(os.path.join(self.out_dir, 'data' + str(file_chunk_count) + '.h5'),
-                       data, compression=None)
+            dd.io.save(get_file_name(i), data, compression=None)
 
-        file_chunk_count = 0
+        n_file_chunks = 0
+        n_chunks_total = 0  # total number of chunks
         current_size = 0  # if this is > approx_file_size make new file chunk
         data = get_empty_data_dict()
         all_coords = self.session.query(Coordinate).all()
         print('{} coordinates choosen to numerify'.format(len(all_coords)))
-        for coord in all_coords:
+        for i, coord in enumerate(all_coords):
             if coord.features:
                 n_masked_bases = 0
                 for is_plus_strand in [True, False]:
@@ -65,19 +69,36 @@ class ExportController(object):
                     for key in ['inputs', 'labels', 'label_masks']:
                         data[key] += coord_data[key]
                     current_size += coord.end
+                    n_chunks_total += len(data['label_masks'])
                     n_masked_bases += sum([len(m) - np.count_nonzero(m)
                                            for m
                                            in coord_data['label_masks']])
                     # break data up in file chunks
                     if current_size * 12 > approx_file_size:
-                        save_data(data, file_chunk_count, shuffle)
+                        save_data(data, n_file_chunks, shuffle)
                         data = get_empty_data_dict()
-                        file_chunk_count += 1
+                        n_file_chunks += 1
                         current_size = 0
                 masked_bases_percent = n_masked_bases / (coord.end * 2) * 100
-                print('Numerified {} of species {} with base level error rate of {:.2f}%'.format(
-                    coord, coord.genome.species, masked_bases_percent))
+                print(('{}/{} Numerified {} of species {} with {} features '
+                       'and a base level error rate of {:.2f}%').format(
+                    i + 1, len(all_coords), coord, coord.genome.species, len(coord.features),
+                    masked_bases_percent))
             else:
-                print('Skipping {} as it has no features'.format(coord))
+                print('{}/{} Skipping {} of species {} as it has no features'.format(
+                    i + 1, len(all_coords), coord, coord.genome.species))
+
         # numerify the left over data
-        save_data(data, file_chunk_count, shuffle)
+        if current_size > 0:
+            print(('Numerified the left over data with a base level '
+                   'error rate of {:.2f}%').format(masked_bases_percent))
+            save_data(data, n_file_chunks, shuffle)
+        else:
+            print('Skipping the left over data as it is empty')
+
+        # add the total n_chunks to the first file
+        file_name = get_file_name(0)
+        f = h5py.File(file_name, 'r+')
+        config = f['config'].attrs
+        config['n_chunks_total'] = n_chunks_total
+        f.close()
