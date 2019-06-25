3e086d78e0294941907676369a4b3437df868527
Felix Stiehler <felix.stiehler87@gmail.com>
2019-06-25 15:11:25 +0200

    direct figure insert in tkinter

--- helixerprep/visualization/visualize.py
+++ helixerprep/visualization/visualize.py
@@ -2,58 +2,50 @@
 import h5py
 import numpy as np
 import tkinter as tk
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 
-class Visualization(tk.Frame):
+class Visualization():
     MARGIN_X = 100
     MARGIN_BOTTOM = 100
-    CANVAS_SIZE_X = 1800 - (2 * MARGIN_X)
-    CANVAS_SIZE_Y = CANVAS_SIZE_X // (16 / 9) - MARGIN_BOTTOM
+    HEATMAP_SIZE_X = 1800 - (2 * MARGIN_X)
+    HEATMAP_SIZE_Y = HEATMAP_SIZE_X // (16 / 9) - MARGIN_BOTTOM
     PIXEL_SIZE = 20
-    BASE_COUNT_X = CANVAS_SIZE_X // PIXEL_SIZE
+    BASE_COUNT_X = HEATMAP_SIZE_X // PIXEL_SIZE
 
-    def __init__(self, data_path, predictions_path):
-        self.data = h5py.File(data_path, 'r')
-        self.predictions = h5py.File(predictions_path, 'r')
+    def __init__(self, root, data_path, predictions_path):
+        self.root = root
+        self.h5_data = h5py.File(data_path, 'r')
+        self.h5_predictions = h5py.File(predictions_path, 'r')
 
-        self.master = tk.Tk()
-        self.canvas = self.init_canvas("#ffffff")
-        self.rects = self.init_rects()
+        self.frame = tk.Frame(self.root)
+        self.frame.pack()
+
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+        canvas = FigureCanvasTkAgg(f, self.root)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
         # GUI elements
-        self.button_frame = tk.Frame(self.master)
-
-        self.show_pred_button = tk.Button(self.button_frame, text='Show Pred')
+        # self.show_pred_button = tk.Button(self.frame, text='Show Pred')
         # self.show_pred_button.bind('<ButtonPress-1>', self.show_pred)
-        self.show_pred_button.pack(side='left')
-        self.button_frame.pack(side='bottom')
+        # self.show_pred_button.pack(side='left')
 
-        self.text = tk.Label(self.master, text=0)
-        self.text.pack(side='bottom')
-
-        self.master.mainloop()
-
-    def init_canvas(self, bg_color):
-        canvas = tk.Canvas(self.master, height=self.CANVAS_SIZE_Y,
-                width=self.CANVAS_SIZE_X)
-        full_rect = canvas.create_rectangle(0, 0, self.CANVAS_SIZE_X,
-                self.CANVAS_SIZE_Y, outline='', fill=bg_color)
-        canvas.pack()
-        return canvas
-
-    def init_rects(self):
-        rects = []
-        for y in range(3):
-            rects.append([])  # one row e.g. representing intron predictions
-            for x in range(self.BASE_COUNT_X):
-                rect = self.canvas.create_rectangle(x * self.PIXEL_SIZE,
-                                                    y * self.PIXEL_SIZE,
-                                                    (x + 1) * self.PIXEL_SIZE,
-                                                    (y + 1) * self.PIXEL_SIZE,
-                                                    outline='')
-                rects[y].append(rect)
-        return rects
+        # self.text = tk.Label(self.frame, text=0)
+        # self.text.pack(side='bottom')
 
 
 if __name__ == '__main__':
-    vis = Visualization(data_path='/home/felix/Desktop/h5_data_1K/validation_data.h5',
+    root = tk.Tk()
+    root.title('root')
+
+    vis = Visualization(root,
+                        data_path='/home/felix/Desktop/h5_data_1K/validation_data.h5',
                         predictions_path='/home/felix/git/HelixerPrep/helixerprep/prediction/predictions.h5')
+
+    root.mainloop()
+
