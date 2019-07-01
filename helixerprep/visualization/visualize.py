df86f4576daccb97982b9143e2733c014990194e
Felix Stiehler <felix.stiehler87@gmail.com>
2019-07-01 15:51:51 +0200

    show sequence error summary in visualization

--- helixerprep/visualization/visualize.py
+++ helixerprep/visualization/visualize.py
@@ -74,17 +74,17 @@ class Visualization():
         self.canvas_main.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         fig_summary = Figure(figsize=(self.HEATMAP_SIZE_X / self.DPI,
-                                      self.HEATMAP_SIZE_X * 3/ self.DPI),
+                                      self.PIXEL_SIZE * 3/ self.DPI),
                      dpi=self.DPI)
         self.ax_summary = fig_summary.add_subplot(111)
         self.canvas_summary = FigureCanvasTkAgg(fig_summary, self.root)
-        # self.canvas_summary.draw()
-        # self.canvas_summary.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.canvas_summary.draw()
+        self.canvas_summary.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        self.redraw()
+        self.redraw(changed_seq=True)
 
-    def load_sequence(self):
-        """loads data for the heatmap and inputs dummy data that serves as margin"""
+    def load_sequence(self, offset, seq_len, include_dummy=True):
+        """loads data for the heatmap and possibly inputs dummy data that serves as margin"""
         def add_dummy_data(dset, new_dset):
             # insert valid data at every second row
             new_dset[0::2,:] = dset.reshape((self.args.n_rows, self.BASE_COUNT_X, 3))
@@ -93,47 +93,52 @@ class Visualization():
             new_dset = new_dset.reshape((self.args.n_rows * 2 - 1) * 3, self.BASE_COUNT_X)
             return new_dset
 
-        off = self.offset
-        off_lim = self.offset + self.BASE_COUNT_SCREEN
-        self.labels = np.array(self.h5_data['/data/y'][self.seq_index][off:off_lim])
+        off_lim = offset + seq_len
+        labels = np.array(self.h5_data['/data/y'][self.seq_index][offset:off_lim])
 
-        self.predictions = np.array(self.h5_predictions['/predictions'][self.seq_index][off:off_lim])
-        self.errors = np.abs(self.labels - self.predictions)
+        predictions = np.array(self.h5_predictions['/predictions'][self.seq_index][offset:off_lim])
+        errors = np.abs(labels - predictions)
 
         dset = self.h5_data['/data/sample_weights']
-        self.label_masks = np.array(dset[self.seq_index][off:off_lim])
-        self.label_masks = np.repeat(self.label_masks[:, np.newaxis], 3, axis=1)
-        self.label_masks = ([1] - self.label_masks).astype(bool)
+        label_masks = np.array(dset[self.seq_index][offset:off_lim])
+        label_masks = np.repeat(label_masks[:, np.newaxis], 3, axis=1)
+        label_masks = ([1] - label_masks).astype(bool)
 
-        # reshape and add dummy data
-        new_labels = np.zeros((self.args.n_rows * 2 - 1, self.BASE_COUNT_X, 3),
-                              dtype=self.labels.dtype)
-        self.labels = add_dummy_data(self.labels, new_labels)
-        new_errors = np.zeros((self.args.n_rows * 2 - 1, self.BASE_COUNT_X, 3),
-                              dtype=self.errors.dtype)
-        self.errors = add_dummy_data(self.errors, new_errors)
-        new_label_masks = np.ones((self.args.n_rows * 2 - 1, self.BASE_COUNT_X, 3)).astype(bool)
-        self.label_masks = add_dummy_data(self.label_masks, new_label_masks)
+        if include_dummy:
+            # reshape and add dummy data
+            new_labels = np.zeros((self.args.n_rows * 2 - 1, self.BASE_COUNT_X, 3),
+                                  dtype=labels.dtype)
+            labels = add_dummy_data(labels, new_labels)
+            new_errors = np.zeros((self.args.n_rows * 2 - 1, self.BASE_COUNT_X, 3),
+                                  dtype=errors.dtype)
+            errors = add_dummy_data(errors, new_errors)
+            new_label_masks = np.ones((self.args.n_rows * 2 - 1, self.BASE_COUNT_X, 3)).astype(bool)
+            label_masks = add_dummy_data(label_masks, new_label_masks)
 
         # make string labels
-        self.labels_str = self.labels.astype(str)
-        self.labels_str[self.labels_str == '0'] = ''
-        self.labels_str[self.labels_str == '1'] = '-'
+        labels_str = labels.astype(str)
+        labels_str[labels_str == '0'] = ''
+        labels_str[labels_str == '1'] = '-'
+
+        return labels, errors, label_masks, labels_str
 
     def draw_main_heatmap(self):
+        labels, errors, label_masks, labels_str = self.load_sequence(self.offset,
+                                                                     self.BASE_COUNT_SCREEN,
+                                                                     include_dummy=True)
         self.ax_main.clear()
         # massively speeds up painting
-        if np.all(self.labels_str == ''):
-            self.labels_str = False
-        seaborn.heatmap(self.errors,
+        if np.all(labels_str == ''):
+            labels_str = False
+        seaborn.heatmap(errors,
                         vmin=0 + args.colorbar_offset,
                         vmax=1 - args.colorbar_offset,
                         cmap='bwr',
                         center=0.5,
                         square=True,
                         cbar=False,
-                        mask=self.label_masks,
-                        annot=self.labels_str,
+                        mask=label_masks,
+                        annot=labels_str,
                         fmt='',
                         annot_kws={'fontweight': 'bold'},
                         xticklabels=False,
@@ -142,26 +147,26 @@ class Visualization():
         self.canvas_main.draw()
 
     def draw_summary_heatmap(self):
-        pass
-        # take masking into account
-        """
+        _, errors, label_masks, _ = self.load_sequence(0, self.chunk_len, include_dummy=False)
+        masked_errors = np.ma.masked_array(errors, mask=label_masks)
+        # reshape and average over each part
+        masked_errors = np.swapaxes(masked_errors, 0, 1)
+        masked_errors = masked_errors.reshape((3, 100, self.chunk_len // 100))
+        masked_errors_avg = np.mean(masked_errors, axis=2)
+        # paint
         self.ax_summary.clear()
-        seaborn.heatmap(self.errors,
-                        vmin=0 + args.colorbar_offset,
-                        vmax=1 - args.colorbar_offset,
+        seaborn.heatmap(masked_errors_avg,
+                        vmin=0,
+                        vmax=1,
                         cmap='bwr',
                         center=0.5,
                         square=True,
                         cbar=False,
-                        mask=self.label_masks,
-                        annot=self.labels_str,
-                        fmt='',
-                        annot_kws={'fontweight': 'bold'},
+                        mask=masked_errors_avg.mask,
                         xticklabels=False,
                         yticklabels=False,
-                        ax=self.ax_main)
-        self.canvas_main.draw()
-        """
+                        ax=self.ax_summary)
+        self.canvas_summary.draw()
 
     def next(self, event):
         self.offset = (self.offset + self.BASE_COUNT_SCREEN) % self.chunk_len
@@ -184,7 +189,7 @@ class Visualization():
         if new_seq_index <= self.n_seq:
             self.seq_index = new_seq_index
             self.offset = 0
-            self.redraw()
+            self.redraw(changed_seq=True)
 
     def go_seq_index(self, event):
         new_seq_index = int(self.seq_index_input.get())
@@ -204,7 +209,6 @@ class Visualization():
     def redraw(self, changed_seq=False):
         self.seq_offset_label.config(text=str('base: {}/{}'.format(self.offset, self.chunk_len - 1)))
         self.seq_index_label.config(text=str('seq: {}/{}'.format(self.seq_index, self.n_seq - 1)))
-        self.load_sequence()
         self.draw_main_heatmap()
         if changed_seq:
             self.draw_summary_heatmap()
