ca0aeae728552ef0b23aae589a9a141933b440e5
Felix Stiehler <felix.stiehler87@gmail.com>
2019-07-09 17:24:14 +0200

    minor

--- scripts/generate_data_and_upload.sh
+++ scripts/generate_data_and_upload.sh
@@ -11,8 +11,8 @@ do
 
 	rm -r "$local_folder"/*
 
-	# ../export.py --db-path-in $db_path --out-dir $local_folder --chunk-size "$size"000 --genomes Creinhardtii,Gmax,Tcacao,Mpolymorpha;
-	../export.py --db-path-in $db_path --out-dir $local_folder --chunk-size "$size"000 --genomes Creinhardtii
+	../export.py --db-path-in $db_path --out-dir $local_folder --chunk-size "$size"000 --genomes Creinhardtii,Gmax,Tcacao,Mpolymorpha
+	# ../export.py --db-path-in $db_path --out-dir $local_folder --chunk-size "$size"000 --genomes Creinhardtii
 
 	rsync -vzr --progress $local_folder felix-stiehler@134.99.200.63:"$remote_main_folder"
 done
