edf68577061a6f996da54c4d5fa5dc1dd0d98999
alisandra <alisandra.denton@hhu.de>
2019-07-17 17:05:53 +0200

    refactored a bit and added some parameters for more flexible comparisons

--- scripts/accs_genic_intergenic.py
+++ scripts/accs_genic_intergenic.py
@@ -8,9 +8,10 @@ from sklearn.metrics import precision_recall_fscore_support as f1_score
 """Outputs the coding and intron accuracy within and outside of genes seperately.
 Needs a data and a corresponding predictions file."""
 
-def append_f1_row(name, col, mask):
-    y = h5_data['/data/y'][:TRUNCATE, :, col][mask]
-    pred = h5_pred['/predictions'][:TRUNCATE, :, col][mask]
+
+def append_f1_row(name, col, mask, h5_data_y, h5_pred_y, table):
+    y = h5_data_y[:, :, col][mask]
+    pred = h5_pred_y[:, :, col][mask]
 
     row = [name]
     scores = f1_score(y, np.round(pred), average='binary', pos_label=1)[:3]
@@ -18,39 +19,41 @@ def append_f1_row(name, col, mask):
     table.append(row)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-data', type=str, required=True)
-parser.add_argument('-predictions', type=str, required=True)
-args = parser.parse_args()
+def main(args):
+    h5_data = h5py.File(args.data, 'r')
+    h5_pred = h5py.File(args.predictions, 'r')
 
-h5_data = h5py.File(args.data, 'r')
-h5_pred = h5py.File(args.predictions, 'r')
+    h5_data_y = h5_data['/data/y'][:args.truncate, :, :]
+    h5_pred_y = h5_pred[args.h5_prediction_dataset][:args.truncate, :, :]
 
-# can be set for faster development
-# TRUNCATE = 10000000
-TRUNCATE = 1000
+    genic_mask = h5_data['/data/y'][:args.truncate, :, 0].astype(bool)
+    error_mask = h5_data['/data/sample_weights'][:args.truncate, :].astype(bool)
+    genic_and_error_mask = np.logical_and(genic_mask, error_mask)
 
-genic_mask = h5_data['/data/y'][:TRUNCATE, :, 0].astype(bool)
-error_mask = h5_data['/data/sample_weights'][:TRUNCATE, :].astype(bool)
-genic_and_error_mask = np.logical_and(genic_mask, error_mask)
+    # cds, intron cols in genic/intergenic
+    for region in ['Genic', 'Intergenic']:
+        table = [['', 'Precision', 'Recall', 'F1-Score']]
+        if region == 'intergenic':
+            current_mask = np.bitwise_not(genic_and_error_mask)
+        else:
+            current_mask = genic_and_error_mask
+        for col, name in [(1, 'coding'), (2, 'intron')]:
+            append_f1_row(name, col, current_mask, h5_data_y, h5_pred_y, table)
+        print(AsciiTable(table, region).table)
 
-# cds, intron cols in genic/intergenic
-for region in ['Genic', 'Intergenic']:
+    # all cols for everything
     table = [['', 'Precision', 'Recall', 'F1-Score']]
-    if region == 'intergenic':
-        current_mask = np.bitwise_not(genic_and_error_mask)
-    else:
-        current_mask = genic_and_error_mask
-    for col, name in [(1, 'coding'), (2, 'intron')]:
-        append_f1_row(name, col, current_mask)
-    print(AsciiTable(table, region).table)
+    for col, name in [(0, 'transcript'), (1, 'coding'), (2, 'intron')]:
+        append_f1_row(name, col, error_mask, h5_data_y, h5_pred_y, table)
+    print(AsciiTable(table, 'Total').table)
 
-# all cols for everything
-table = [['', 'Precision', 'Recall', 'F1-Score']]
-for col, name in [(0, 'transcript'), (1, 'coding'), (2, 'intron')]:
-    append_f1_row(name, col, error_mask)
-print(AsciiTable(table, 'Total').table)
 
-# everything
-# todo
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, required=True)
+    parser.add_argument('--predictions', type=str, required=True)
+    parser.add_argument('--truncate', type=int, default=-1, help="can set to e.g. 1000 for development speed")
+    parser.add_argument('--h5_prediction_dataset', type=str, default='/predictions',
+                        help="dataset in predictions h5 file to compare with data's '/data/y', default='/predictions',"
+                             "the other likely option is '/data/y'")
+    main(parser.parse_args())
