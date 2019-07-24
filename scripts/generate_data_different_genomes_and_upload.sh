494db244cd8a0f12a5cdf04ebafdc9cdc274c680
Felix Stiehler <felix.stiehler87@gmail.com>
2019-07-24 13:26:12 +0200

    script to generate and upload test set data from each genome individually

---
+++ scripts/generate_data_different_genomes_and_upload.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+db_path='/home/felix/Desktop/full_geenuff.sqlite3'
+local_main_folder_input="/home/felix/git/GeenuFF/tmp/"
+local_main_folder_output="/home/felix/Desktop/data/single_genomes/"
+remote_main_folder="/dev/shm/helixer/data/single_genomes/"
+
+chunk_size=10000
+coordinate_chance=1.0
+
+for genome in $(ls -1 $local_main_folder_input)
+do
+	echo -e "\nGenerating $genome"
+	../export.py --db-path-in $db_path --out-dir "$local_main_folder_output$genome" --chunk-size $chunk_size --coordinate-chance $coordinate_chance --genomes $genome --only-test-set
+
+	echo -e "\nSending $genome"
+	rsync -vz --progress "$local_main_folder_output$genome/test_data.h5" felix-stiehler@134.99.200.63:"$remote_main_folder$genome/"
+done
