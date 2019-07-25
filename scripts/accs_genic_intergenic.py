c4794bc6c7230706a58cb86a5ef7e55ab16bc439
alisandra <alisandra.denton@hhu.de>
2019-07-25 16:53:46 +0200

    can now save to file

--- scripts/accs_genic_intergenic.py
+++ scripts/accs_genic_intergenic.py
@@ -16,17 +16,21 @@ def main(args):
 
     # get comparable subset of data
     if not args.unsorted or all_coords_match(h5_data, h5_pred):
-        h5_data_y = h5_data['/data/y']
-        h5_pred_y = h5_pred[args.h5_prediction_dataset]
+        h5_data_y = np.array(h5_data['/data/y'])
+        h5_pred_y = np.array(h5_pred[args.h5_prediction_dataset])
+        lab_mask = [True] * h5_data_y.shape[0]
+        lab_lexsort = np.arange(h5_data_y.shape[0])
     else:
-        h5_data_y, h5_pred_y = match_up(h5_data, h5_pred, args.h5_prediction_dataset)
+        h5_data_y, h5_pred_y, lab_mask, lab_lexsort = match_up(h5_data, h5_pred, args.h5_prediction_dataset)
 
     # truncate (for devel efficiency, when we don't need the whole answer)
     if args.truncate is not None:
+        assert args.save_to is None, "truncate and save not implemented"
         h5_data_y = h5_data_y[:args.truncate]
         h5_pred_y = h5_pred_y[:args.truncate]
     # random subset (for devel efficiency, or just if we don't care that much about the full accuracy
     if args.sample is not None:
+        assert args.save_to is None, "sample and save not implemented"
         a_sample = np.random.choice(
             np.arange(h5_data_y.shape[0]),
             size=[args.sample],
@@ -34,6 +38,13 @@ def main(args):
         )
         h5_data_y = h5_data_y[a_sample]
         h5_pred_y = h5_pred_y[a_sample]
+
+    # export the cleaned up matched up everything
+    if args.save_to is not None:
+
+        export(args.save_to, h5_in=h5_data,
+               labs=h5_data_y, preds=h5_pred_y,
+               lab_mask=lab_mask, lab_lexsort=lab_lexsort)
 
     # and score
     f1_calc = F1Calculator(None, None)
@@ -76,10 +87,38 @@ def match_up(h5_data, h5_pred, h5_prediction_dataset):
     sorting_matches = (shared_lab_keys == shared_pred_keys).all()
     # resort both if not
     if not sorting_matches:
-        labs = labs[np.lexsort(np.flip(shared_lab_keys.T, axis=0))]
+        lab_lexsort = np.lexsort(np.flip(shared_lab_keys.T, axis=0))
+        labs = labs[lab_lexsort]
         preds = preds[np.lexsort(np.flip(shared_pred_keys.T, axis=0))]
+    else:
+        lab_lexsort = np.arange(labs.shape[0])
     # todo, option to save as h5?
-    return labs, preds
+    return labs, preds, lab_mask, lab_lexsort
+
+
+def export(h5_path, h5_in, labs, preds, lab_mask, lab_lexsort):
+    h5_file = h5py.File(h5_path, 'w')
+    # setup datasets
+    for key in h5_in['data'].keys():
+        dset = h5_in['data/' + key]
+        shape = list(dset.shape)
+        shape[0] = labs.shape[0]
+        h5_file.create_dataset('data/' + key,
+                               shape=shape,
+                               dtype=dset.dtype,
+                               compression="lzf"
+                               )
+        if key != "y":
+            cleanup = np.array(dset)[lab_mask]
+            cleanup = cleanup[lab_lexsort]
+            h5_file['data/' + key][:] = cleanup
+    h5_file['data/y'][:] = labs
+    h5_file.create_dataset('predictions',
+                           shape=preds.shape,
+                           dtype='int8',
+                           compression='lzf')
+    h5_file['predictions'][:] = preds
+    h5_file.close()
 
 
 def mk_keys(h5):
@@ -90,7 +129,7 @@ def mk_keys(h5):
 
 
 def print_accuracy_counts(y, preds):
-    print("overall accuracy: {:06.4f}%".format(acc_percent(y, preds)))
+    print("overall accuracy: {:06.4f}%".format(acc_percent(y[:], preds[:])))
     print("transcriptional accuracy: {:06.4f}%".format(acc_percent(y[:, :, 0], preds[:, :, 0])))
     print("coding accuracy: {:06.4f}%".format(acc_percent(y[:, :, 1], preds[:, :, 1])))
     print("intron accuracy: {:06.4f}%".format(acc_percent(y[:, :, 2], preds[:, :, 2])))
@@ -108,5 +147,6 @@ if __name__ == "__main__":
                         help="don't assume coordinates match up but use the h5 datasets [species, seqids, start_ends]"
                              "to check order and reorder as necessary")
     parser.add_argument('--sample', type=int, default=None,
-                        help="take a random sample of the data of this many chuncks")
+                        help="take a random sample of the data of this many chunks")
+    parser.add_argument('--save_to', type=str, help="set this to output the newly sorted matches to a h5 file")
     main(parser.parse_args())
