c2f34202c7b68158e528e191cca524ee2b9b0ac6
Felix Stiehler <felix.stiehler87@gmail.com>
2019-07-29 14:24:24 +0200

    minor

--- scripts/make_csv_from_single_genome_eval.py
+++ scripts/make_csv_from_single_genome_eval.py
@@ -6,7 +6,7 @@ nni_base = '/home/felix-stiehler/nni/experiments'
 nni_id = 'jaeWb7Ff'
 trials_folder = '{}/{}/trials'.format(nni_base, nni_id)
 
-print(','.join(['genome', 'loss', 'acc_overall', 'acc_transcript', 'acc_coding', 'acc_intron']))
+print(','.join(['genome', 'loss', 'acc_overall', 'acc_transcript', 'acc_coding', 'acc_intron', 'nni id']))
 for folder in os.listdir(trials_folder):
     # get genome name
     parameters = eval(open('{}/{}/parameter.cfg'.format(trials_folder, folder)).read())
@@ -18,4 +18,4 @@ for folder in os.listdir(trials_folder):
     for line in log_file:
         pass
     results = eval(''.join(line.strip().split(' ')[4:]))
-    print(','.join([genome] + ['{:.4f}'.format(r) for r in results.values()]))
+    print(','.join([genome] + ['{:.4f}'.format(r) for r in results.values()] + [folder]))
