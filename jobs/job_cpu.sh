7072d1abc72a70a067a3d1d4ef7432c49f8b2017
Felix Stiehler <felix.stiehler87@gmail.com>
2019-07-29 11:18:16 +0200

    cluster job files

--- jobs/job_cpu.sh
+++ jobs/job_cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=2:mem=10gb:ngpus=2:accelerator_model=gtx1080ti
-#PBS -l walltime=20:59:00
+#PBS -l select=1:ncpus=1:mem=30gb:arch=skylake
+#PBS -l walltime=12:59:00
 #PBS -A "HelixerOpt"
 set -e
 
@@ -18,10 +18,6 @@ echo "`date +"%d.%m.%Y-%T"`" >> $LOGFILE
 
 ##Software-Umgebung laden
 module load Python/3.6.5
-pip3 install --user terminaltables
-
-module load TensorFlow/1.10.0
-module load Keras/2.2.4_python
 
 ##Daten vom Arbeitsverzeichnis auf das Scratch-Laufwerk kopieren
 cp -r $PBS_O_WORKDIR/* $SCRATCHDIR/.
@@ -29,7 +25,7 @@ cd $SCRATCHDIR
 rm $PBS_JOBNAME"."$PBS_JOBID".log"
 
 ##Aufruf
-/home/festi100/git/HelixerPrep/helixerprep/prediction/LSTMModel.py -v -p 2 -e 10 -u 64 -bs 64 -lr 0.001 --bidirectional -fp float64 -d /scratch/festi100/four_genomes/h5_data_5k --gpus 2
+rm -r /scratch_gs/festi100/data/all_genomes_full/h5_data_10k/*; /home/festi100/git/HelixerPrep/export.py --db-path-in /scratch_gs/festi100/full_geenuff.sqlite3 --out-dir /scratch_gs/festi100/data/all_genomes_full/h5_data_10k --chunk-size 10000 --exclude-genomes Dsalina
 
 ##Daten zurück kopieren
 cp -r "$SCRATCHDIR"/* $PBS_O_WORKDIR/.
