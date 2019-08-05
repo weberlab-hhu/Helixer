180422ba96e946060fe92486d0c9b05dfe2e0b61
Felix Stiehler <felix.stiehler87@gmail.com>
2019-08-05 09:55:15 +0200

    dropout params added to danq

--- helixerprep/prediction/DanQModel.py
+++ helixerprep/prediction/DanQModel.py
@@ -2,7 +2,7 @@
 import random
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Conv1D, LSTM, CuDNNLSTM, Dense, Bidirectional, MaxPooling1D
+from keras.layers import Conv1D, LSTM, CuDNNLSTM, Dense, Bidirectional, MaxPooling1D, Dropout
 from HelixerModel import HelixerModel, get_col_accuracy_fn
 
 
@@ -13,7 +13,9 @@ class DanQModel(HelixerModel):
         self.parser.add_argument('-u', '--units', type=int, default=4)
         self.parser.add_argument('-f', '--filter-depth', type=int, default=8)
         self.parser.add_argument('-ks', '--kernel-size', type=int, default=26)
-        self.parser.add_argument('-ps', '--pool-size', type=int, default=1)
+        self.parser.add_argument('-ps', '--pool-size', type=int, default=10)
+        self.parser.add_argument('-dr1', '--dropout1', type=float, default=0.0)
+        self.parser.add_argument('-dr2', '--dropout2', type=float, default=0.0)
         self.parse_args()
 
     def model(self):
@@ -27,7 +29,10 @@ class DanQModel(HelixerModel):
         if self.pool_size > 1:
             model.add(MaxPooling1D(pool_size=self.pool_size, padding='same'))
 
+        model.add(Dropout(self.dropout1))
         model.add(Bidirectional(CuDNNLSTM(self.units, return_sequences=True)))
+
+        model.add(Dropout(self.dropout2))
         model.add(Dense(self.pool_size * 3, activation='sigmoid'))
         return model
 
