2ffa97f7d8f2e8d44e5e1dc94325bcb4a7f81938
Felix Stiehler <felix.stiehler87@gmail.com>
2019-09-13 14:18:02 +0200

    single genome eval script

--- scripts/make_csv_from_single_genome_eval.py
+++ scripts/make_csv_from_single_genome_eval.py
@@ -2,29 +2,38 @@
 
 import os
 
-nni_base = '/home/felix-stiehler/nni/experiments'
-nni_id = 'jaeWb7Ff'
+nni_base = '/home/felix/nni/experiments'
+nni_id = 'adPNZ2sW'
 trials_folder = '{}/{}/trials'.format(nni_base, nni_id)
 
-print(','.join(['genome', 'loss', 'acc_overall', 'f1_total_0', 'f1_total_1', 'nni_id']))
+print(','.join(['genome', 'loss', 'acc_overall', 'acc_genic', 'acc_intergenic', 'acc_UTR',
+                'acc_CDS', 'acc_intron', 'nni_id']))
 for folder in os.listdir(trials_folder):
     # get genome name
     parameters = eval(open('{}/{}/parameter.cfg'.format(trials_folder, folder)).read())
     path = parameters['parameters']['test_data']
     genome = path.split('/')[6]
 
-    # get loss and overall acc
+    # parse confusion matrix
     log_file = open('{}/{}/trial.log'.format(trials_folder, folder))
-    total_count = 0
     for line in log_file:
-        if line.startswith('| total 0'):
-            total_count += 1
-            if total_count == 3:
-                # overall total
-                f1_total_0 = line.split()[8]
-                line = next(log_file)
-                f1_total_1 = line.split()[8]
+        if 'array([[' in line:  # start of cm
+            cm_str = line.split('array(')[1].strip()
+            for i in range(3):
+                cm_str += next(log_file).strip()
+            cm_str = cm_str[:-1]  # remove last round bracket
+    cm = eval(cm_str)
+    acc_ig, acc_UTR, acc_CDS, acc_intron = cm[0][0], cm[1][1], cm[2][2], cm[3][3]
 
+    # parse metrics
     overall_results = eval(''.join(line.strip().split(' ')[4:]))
-    numbers = [overall_results['loss'], overall_results['acc']]
-    print(','.join([genome] + ['{:.4f}'.format(n) for n in numbers] + [f1_total_0, f1_total_1, folder]))
+    numbers = [
+        overall_results['loss'],
+        overall_results['main_output_acc'],
+        overall_results['main_output_acc_g_oh'],
+        acc_ig,
+        acc_UTR,
+        acc_CDS,
+        acc_intron
+    ]
+    print(','.join([genome] + ['{:.4f}'.format(n) for n in numbers] + [folder]))
