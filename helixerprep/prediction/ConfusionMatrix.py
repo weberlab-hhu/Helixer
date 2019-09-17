more cm fixes, zero pad masking in custom metrics

helixerprep/prediction/ConfusionMatrix.py
@@ -26,15 +26,11 @@ class ConfusionMatrix():
         y_true = self._reshape_data(y_true)
         self.cm += confusion_matrix(y_true, y_pred, labels=range(self.label_dim))
 
-    def _normalize_cm(self):
+    def _get_normalized_cm(self):
         """Put in extra function to be testable"""
         class_sums = np.sum(self.cm, axis=1)
-        self.cm /= class_sums[:, None]  # expand by one dim so broadcast work properly
-
-    def _print_cm(self):
-        print()
-        pprint(self.cm)
-        print()
+        normalized_cm = self.cm / class_sums[:, None]  # expand by one dim so broadcast work properly
+        return normalized_cm
 
     def calculate_cm(self, model):
         for i in range(len(self.generator)):
@@ -47,9 +43,10 @@ class ConfusionMatrix():
                 y_pred, meta_pred = y_pred
                 y_true, meta_true = y_true
 
-            self._add_to_cm(y_true, y_pred, X)
+            self._add_to_cm(y_true, y_pred)
 
-        self._print_cm()
-        self._normalize_cm()
-        self._print_cm()
-
+        print()
+        pprint(self.cm)
+        print()
+        pprint(self._get_normalized_cm())
+        print()

helixerprep/prediction/HelixerModel.py
@@ -31,9 +31,13 @@ from ConfusionMatrix import ConfusionMatrix
 
 
 def acc_region(y_true, y_pred, col, value):
-    mask = tf.equal(y_true[:, :, :, col], tf.constant(value))
+    zero_pad_mask = K.all(tf.equal(y_true, tf.constant(0.0)), axis=-1)
+    content_mask = tf.equal(y_true[:, :, :, col], tf.constant(value))
+    mask = tf.logical_or(zero_pad_mask, content_mask)
+
     y_true = K.argmax(tf.boolean_mask(y_true, mask), axis=-1)
     y_pred = K.argmax(tf.boolean_mask(y_pred, mask), axis=-1)
+
     error = K.cast(K.equal(y_true, y_pred), K.floatx())
     error_return = tf.cond(tf.equal(tf.size(error), 0),
                            lambda: tf.constant(0.0), lambda: error)
@@ -61,20 +65,22 @@ class ReportIntermediateResult(Callback):
 # is buggy currently
 class ConfusionMatrixTest(Callback):
     def __init__(self, generator, label_dim):
-        self.cm_calculator = ConfusionMatrix(generator, label_dim)
-        super(ConfusionMatrixTest, self).__init__()
+        self.generator = generator
+        self.label_dim = label_dim
 
     def on_test_end(self, logs=None):
-        self.cm_calculator.calculate_cm(self.model)
+        cm_calculator = ConfusionMatrix(self.generator, self.label_dim)
+        cm_calculator.calculate_cm(self.model)
 
 
 class ConfusionMatrixTrain(Callback):
     def __init__(self, generator, label_dim):
-        self.cm_calculator = ConfusionMatrix(generator, label_dim)
-        super(ConfusionMatrixTrain, self).__init__()
+        self.generator = generator
+        self.label_dim = label_dim
 
     def on_epoch_end(self, epoch, logs=None):
-        self.cm_calculator.calculate_cm(self.model)
+        cm_calculator = ConfusionMatrix(self.generator, self.label_dim)
+        cm_calculator.calculate_cm(self.model)
 
 
 class HelixerSequence(Sequence):
@@ -116,8 +122,8 @@ class HelixerSequence(Sequence):
         assert np.all(np.logical_and(self.coord_lengths >= 0.0, self.coord_lengths <= 1.0))
 
     def __len__(self):
-        # return 2
-        return int(np.ceil(len(self.usable_idx) / float(self.batch_size)))
+        return 2
+        # return int(np.ceil(len(self.usable_idx) / float(self.batch_size)))
 
     @abstractmethod
     def __getitem__(self, idx):

helixerprep/tests/test_helixer_prep.py
@@ -899,5 +899,4 @@ def test_confusion_matrix():
         [0, 0, 1 / 2, 1 / 2]
     ])
 
-    cm._normalize_cm()
-    assert np.allclose(cm_true_normalized, cm.cm)
+    assert np.allclose(cm_true_normalized, cm._get_normalized_cm())