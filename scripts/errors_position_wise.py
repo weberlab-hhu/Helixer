f015025e5e004352008d8d90077d71ad5a2953f9
Felix Stiehler <felix.stiehler87@gmail.com>
2019-10-22 15:00:45 +0200

    plot error position values

--- scripts/errors_position_wise.py
+++ scripts/errors_position_wise.py
@@ -2,6 +2,7 @@
 import h5py
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
 from terminaltables import AsciiTable
 from helixerprep.prediction.ConfusionMatrix import ConfusionMatrix
 
@@ -29,6 +30,7 @@ else:
 # this can not be done at once if there is not enough memory
 y_diff = np.argmax(y_true, axis=-1) == np.argmax(y_pred, axis=-1)
 
+total_accs, genic_f1s, offsets = [], [], []
 table = [['index', 'overall acc']]
 for i in range(0, y_true.shape[1], args.resolution):
     y_true_section = y_true[:, i:i+args.resolution]
@@ -36,9 +38,17 @@ for i in range(0, y_true.shape[1], args.resolution):
     y_diff_section = y_diff[:, i:i+args.resolution]
     overall_acc = np.count_nonzero(y_diff_section) / np.product(y_diff_section.shape) * 100
     table.append(f'{i}\t{overall_acc:.4f}'.split('\t'))
+    total_accs.append(overall_acc / 100.0)
     # cm
     print(f'\n{i}/{y_true.shape[1]}')
     cm = ConfusionMatrix(None, 4)
     cm._add_to_cm(y_true_section, y_pred_section)
-    cm._print_results()
+    genic_f1s.append(cm._print_results())
+    offsets.append(i)
 print('\n', AsciiTable(table).table, sep='')
+
+plt.plot(offsets, total_accs, label='overall acc')
+plt.plot(offsets, genic_f1s, label='genic f1')
+plt.xlabel('length offset')
+plt.legend()
+plt.show()
