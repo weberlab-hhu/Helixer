6b8bc3e944a1233295ddbb32487542b15087d6c2
Felix Stiehler <felix.stiehler87@gmail.com>
2019-10-22 11:53:41 +0200

    postion wise error script updated for new data

--- scripts/errors_position_wise.py
+++ scripts/errors_position_wise.py
@@ -4,17 +4,11 @@ import numpy as np
 import argparse
 from terminaltables import AsciiTable
 
-def overall_acc_percent(y_true, y_pred):
-    return np.count_nonzero(y_true == y_pred) / np.product(y_true.shape) * 100
-
-def rowwise_acc_percent(y_true, y_pred):
-    return np.count_nonzero((y_true == y_pred).all(axis=2)) / np.product(y_true.shape[:2]) * 100
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--data', type=str, required=True)
 parser.add_argument('-p', '--predictions', type=str, required=True)
 parser.add_argument('-s', '--sample', type=int, default=None)
-parser.add_argument('-res', '--resolution', type=int, default=100)
+parser.add_argument('-res', '--resolution', type=int, default=1000)
 args = parser.parse_args()
 
 h5_data = h5py.File(args.data, 'r')
@@ -30,11 +24,13 @@ else:
     y_true = h5_data['/data/y']
     y_pred = h5_pred['/predictions']
 
-table = [['index', 'acc pointwise', 'acc rowwise']]
+y_true = np.argmax(y_true, axis=-1)
+y_pred = np.argmax(y_pred, axis=-1)
+y_diff = y_true == y_pred
+
+table = [['index', 'overall acc']]
 for i in range(0, y_true.shape[1], args.resolution):
-    y_true_section = y_true[:, i:i+args.resolution]
-    y_pred_section = y_pred[:, i:i+args.resolution]
-    overall_acc = overall_acc_percent(y_true_section, np.round(y_pred_section))
-    rowwise_acc = rowwise_acc_percent(y_true_section, np.round(y_pred_section))
-    table.append('{}\t{:.4f}\t{:.4f}'.format(i, overall_acc, rowwise_acc).split('\t'))
+    y_diff_section = y_diff[:, i:i+args.resolution]
+    overall_acc = np.count_nonzero(y_diff_section) / np.product(y_diff_section.shape) * 100
+    table.append(f'{i}\t{overall_acc:.4f}'.split('\t'))
 print('\n', AsciiTable(table).table, sep='')
