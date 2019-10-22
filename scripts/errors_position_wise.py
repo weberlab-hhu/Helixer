c771ab0ecdc22c8e2db753673fafa93c0582da54
Felix Stiehler <felix.stiehler87@gmail.com>
2019-10-22 13:19:50 +0200

    output confusion matrix in errors_position_wise.py

--- scripts/errors_position_wise.py
+++ scripts/errors_position_wise.py
@@ -3,6 +3,8 @@ import h5py
 import numpy as np
 import argparse
 from terminaltables import AsciiTable
+from helixerprep.prediction.ConfusionMatrix import ConfusionMatrix
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--data', type=str, required=True)
@@ -24,13 +26,19 @@ else:
     y_true = h5_data['/data/y']
     y_pred = h5_pred['/predictions']
 
-y_true = np.argmax(y_true, axis=-1)
-y_pred = np.argmax(y_pred, axis=-1)
-y_diff = y_true == y_pred
+# this can not be done at once if there is not enough memory
+y_diff = np.argmax(y_true, axis=-1) == np.argmax(y_pred, axis=-1)
 
 table = [['index', 'overall acc']]
 for i in range(0, y_true.shape[1], args.resolution):
+    y_true_section = y_true[:, i:i+args.resolution]
+    y_pred_section = y_pred[:, i:i+args.resolution]
     y_diff_section = y_diff[:, i:i+args.resolution]
     overall_acc = np.count_nonzero(y_diff_section) / np.product(y_diff_section.shape) * 100
     table.append(f'{i}\t{overall_acc:.4f}'.split('\t'))
+    # cm
+    print(f'\n{i}/{y_true.shape[1]}')
+    cm = ConfusionMatrix(None, 4)
+    cm._add_to_cm(y_true_section, y_pred_section)
+    cm._print_results()
 print('\n', AsciiTable(table).table, sep='')
