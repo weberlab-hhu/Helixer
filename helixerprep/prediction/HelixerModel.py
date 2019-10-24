1c44193215dd56991d2dfaefb9dceba85386dbcb
Felix Stiehler <felix.stiehler87@gmail.com>
2019-10-24 13:32:50 +0200

    enhanced single genome eval script, put old metrics into HelixerModel temporarily

--- helixerprep/prediction/HelixerModel.py
+++ helixerprep/prediction/HelixerModel.py
@@ -31,6 +31,27 @@ from F1Scores import F1Calculator
 from ConfusionMatrix import ConfusionMatrix
 
 
+def acc_region(y_true, y_pred, col, value):
+    non_zero_pad_mask = K.any(tf.equal(y_true, tf.constant(1.0)), axis=-1)
+    content_mask = tf.equal(y_true[:, :, :, col], tf.constant(value))
+    mask = tf.logical_and(non_zero_pad_mask, content_mask)
+
+    y_true = K.argmax(tf.boolean_mask(y_true, mask), axis=-1)
+    y_pred = K.argmax(tf.boolean_mask(y_pred, mask), axis=-1)
+
+    errors = K.cast(K.equal(y_true, y_pred), K.floatx())
+    error_return = tf.cond(tf.equal(tf.size(errors), 0),
+                           lambda: tf.constant(0.0), lambda: K.mean(errors))
+    return error_return
+
+
+def acc_g_oh(y_true, y_pred):
+    return acc_region(y_true, y_pred, 0, 0.0)
+
+
+def acc_ig_oh(y_true, y_pred):
+    return acc_region(y_true, y_pred, 0, 1.0)
+
 # Callbacks have to be done seperately for train/test as the way they are called by Keras
 # is buggy currently
 class ConfusionMatrixTest(Callback):
@@ -343,6 +364,8 @@ class HelixerModel(ABC):
     def _load_helixer_model(self):
         model = load_model(self.load_model_path, custom_objects = {
             'LayerNormalization': LayerNormalization,
+            'acc_g_oh': acc_g_oh,
+            'acc_ig_oh': acc_ig_oh,
         })
         return model
 
--- scripts/make_csv_from_single_genome_eval.py
+++ scripts/make_csv_from_single_genome_eval.py
@@ -1,17 +1,30 @@
 #! /usr/bin/env python3
 import os
-import numpy as np
 import h5py
+import numpy as np
+import argparse
 from helixerprep.prediction.ConfusionMatrix import ConfusionMatrix
 
-nni_base = '/mnt/data/experiments_backup/nni_clc_server/nni/experiments/'
-nni_id = 'XWPVurip'
-trials_folder = '{}/{}/trials'.format(nni_base, nni_id)
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--server', type=str, default='clc')
+parser.add_argument('-nni', '--nni-id', type=str, required=True)
+parser.add_argument('-i', '--ignore', action='append')
+args = parser.parse_args()
 
+assert args.server in ['clc', 'cluster']
+assert len(args.nni_id) == 8
+
+if args.server == 'clc':
+    nni_base = '/mnt/data/experiments_backup/nni_clc_server/nni/experiments/'
+else:
+    nni_base = '/mnt/data/experiments_backup/nni_cluster/nni/experiments/'
+
+trials_folder = '{}/{}/trials'.format(nni_base, args.nni_id)
 print(','.join(['genome', 'loss', 'acc_overall', 'f1_ig', 'f1_utr', 'f1_exon', 'f1_intron',
-                'f1_cds', 'f1_genic', 'old_f1_cds_1', 'error_rate', 'nni_id']))
+                'f1_cds', 'f1_genic', 'old_f1_cds_1', 'base_level_error_rate', 'padded_bases_rate',
+                'sequence_error_rate', 'nni_id']))
 for folder in os.listdir(trials_folder):
-    if folder in ['g8EKR', 'rPxUg']:
+    if folder in args.ignore:
         continue
     # get genome name
     parameters = eval(open('{}/{}/parameter.cfg'.format(trials_folder, folder)).read())
@@ -19,12 +32,27 @@ for folder in os.listdir(trials_folder):
     # genome = path.split('/')[5]  # when from cluster
     genome = path.split('/')[6]
 
-    # get error rate
+    # get sequence error rate
     f = h5py.File('/home/felix/Desktop/data/single_genomes/' + genome + '/h5_data_20k/test_data.h5')
     n_samples = f['/data/X'].shape[0]
     err = np.array(f['/data/err_samples'])
     n_err_samples = np.count_nonzero(err == True)
-    error_rate = n_err_samples / n_samples
+    sequence_error_rate = n_err_samples / n_samples
+
+    # get base level error rate (including padding) iterativly to avoid running into memory issues
+    sw_dset = f['/data/sample_weights']
+    y_dset = f['/data/y']
+    step_size = 1000
+    n_error_bases = 0
+    n_padded_bases = 0
+    idxs = np.array_split(np.arange(len(sw_dset)), len(sw_dset) // step_size)
+    for slice_idxs in idxs:
+        sw_slice = sw_dset[list(slice_idxs)]
+        y_slice = y_dset[list(slice_idxs)]
+        n_error_bases += np.count_nonzero(sw_slice == 0)
+        n_padded_bases += np.count_nonzero(np.all(y_slice == 0, axis=-1))
+    base_level_error_rate = n_error_bases / sw_dset.size
+    padded_bases_rate = n_padded_bases / sw_dset.size
 
     log_file = open('{}/{}/trial.log'.format(trials_folder, folder))
     # get confusion matrix from log to calculate cds f1 that is the same as before
@@ -61,5 +89,6 @@ for folder in os.listdir(trials_folder):
     keras_metrics = eval(''.join(line.strip().split(' ')[4:]))
     selected_keras_metrics = [keras_metrics['loss'], keras_metrics['acc']]
     str_rows = [genome] + ['{:.4f}'.format(n) for n in selected_keras_metrics] + f1_scores
-    str_rows += ['{:.4f}'.format(n) for n in [old_f1_cds_1, error_rate]] + [folder]
+    other_numbers = [old_f1_cds_1, base_level_error_rate, padded_bases_rate, sequence_error_rate]
+    str_rows += ['{:.4f}'.format(n) for n in other_numbers] + [folder]
     print(','.join(str_rows))
