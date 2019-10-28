b755c1c127faa3f25b7b678a82835a805cbab7a7
Felix Stiehler <felix.stiehler87@gmail.com>
2019-10-28 10:00:23 +0100

    error rate calculation optional in csv script

--- scripts/make_csv_from_single_genome_eval.py
+++ scripts/make_csv_from_single_genome_eval.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--server', type=str, default='clc')
 parser.add_argument('-nni', '--nni-id', type=str, required=True)
 parser.add_argument('-i', '--ignore', action='append')
+parser.add_argument('-er', '--error-rates', action='store_true')
 args = parser.parse_args()
 
 assert args.server in ['clc', 'cluster']
@@ -18,10 +19,15 @@ if args.server == 'clc':
     nni_base = '/mnt/data/experiments_backup/nni_clc_server/nni/experiments/'
 else:
     nni_base = '/mnt/data/experiments_backup/nni_cluster/nni/experiments/'
-
 trials_folder = '{}/{}/trials'.format(nni_base, args.nni_id)
-print(','.join(['genome', 'acc_overall', 'f1_ig', 'f1_utr', 'f1_exon', 'f1_intron', 'legacy_f1_cds',
-                'f1_genic', 'base_level_error_rate', 'padded_bases_rate', 'sequence_error_rate', 'nni_id']))
+
+header = ['genome', 'acc_overall', 'f1_ig', 'f1_utr', 'f1_exon', 'f1_intron', 'legacy_f1_cds',
+          'f1_genic']
+if args.error_rates:
+    header += ['base_level_error_rate', 'padded_bases_rate', 'sequence_error_rate']
+header += ['nni_id']
+print(','.join(header))
+
 for folder in os.listdir(trials_folder):
     if args.ignore and folder in args.ignore:
         continue
@@ -39,19 +45,20 @@ for folder in os.listdir(trials_folder):
     sequence_error_rate = n_err_samples / n_samples
 
     # get base level error rate (including padding) iterativly to avoid running into memory issues
-    sw_dset = f['/data/sample_weights']
-    y_dset = f['/data/y']
-    step_size = 1000
-    n_error_bases = 0
-    n_padded_bases = 0
-    idxs = np.array_split(np.arange(len(sw_dset)), len(sw_dset) // step_size)
-    for slice_idxs in idxs:
-        sw_slice = sw_dset[list(slice_idxs)]
-        y_slice = y_dset[list(slice_idxs)]
-        n_error_bases += np.count_nonzero(sw_slice == 0)
-        n_padded_bases += np.count_nonzero(np.all(y_slice == 0, axis=-1))
-    base_level_error_rate = n_error_bases / sw_dset.size
-    padded_bases_rate = n_padded_bases / sw_dset.size
+    if args.error_rates:
+        sw_dset = f['/data/sample_weights']
+        y_dset = f['/data/y']
+        step_size = 1000
+        n_error_bases = 0
+        n_padded_bases = 0
+        idxs = np.array_split(np.arange(len(sw_dset)), len(sw_dset) // step_size)
+        for slice_idxs in idxs:
+            sw_slice = sw_dset[list(slice_idxs)]
+            y_slice = y_dset[list(slice_idxs)]
+            n_error_bases += np.count_nonzero(sw_slice == 0)
+            n_padded_bases += np.count_nonzero(np.all(y_slice == 0, axis=-1))
+        base_level_error_rate = n_error_bases / sw_dset.size
+        padded_bases_rate = n_padded_bases / sw_dset.size
 
     # parse metric table
     log_file = open('{}/{}/trial.log'.format(trials_folder, folder))
@@ -73,6 +80,8 @@ for folder in os.listdir(trials_folder):
 
     # merge everything into one string
     str_rows = [genome, acc_overall] + f1_scores
-    other_numbers = [base_level_error_rate, padded_bases_rate, sequence_error_rate]
-    str_rows += ['{:.4f}'.format(n) for n in other_numbers] + [folder]
+    if args.error_rates:
+        error_rates = [base_level_error_rate, padded_bases_rate, sequence_error_rate]
+        str_rows += ['{:.4f}'.format(n) for n in error_rates]
+    str_rows += [folder]
     print(','.join(str_rows))
