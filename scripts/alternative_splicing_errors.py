9f23e32427a00b14546349ee94411d3513c83c43
Felix Stiehler <felix.stiehler87@gmail.com>
2019-11-26 16:08:28 +0100

    splicing script seems done but is buggy

--- scripts/alternative_splicing_errors.py
+++ scripts/alternative_splicing_errors.py
@@ -10,7 +10,6 @@ from intervaltree import IntervalTree
 from terminaltables import AsciiTable
 from helixerprep.prediction.ConfusionMatrix import ConfusionMatrix
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--data', type=str, required=True,
                     help='This HAS to be from only one genome')
@@ -18,26 +17,32 @@ parser.add_argument('-p', '--predictions', type=str, required=True,
                     help='This HAS to be from only one genome')
 parser.add_argument('-db', '--db-path', type=str, required=True)
 parser.add_argument('-g', '--genome', type=str, required=True)
+parser.add_argument('-o', '--output-file', type=str, default='alternative_splicing_results')
 args = parser.parse_args()
 
 h5_data = h5py.File(args.data, 'r')
 h5_pred = h5py.File(args.predictions, 'r')
+
+y_true = h5_data['/data/y']
+y_pred = h5_pred['/predictions']
+sw = h5_data['/data/sample_weights']
 seqids = np.array(h5_data['/data/seqids'])
 start_ends = np.array(h5_data['/data/start_ends'])
 
 # first fill a dict of intervaltrees with the start_end values of the data
-d = defaultdict(lambda: {'plus': IntervalTree(), 'minus': IntervalTree()})
+intervals = defaultdict(lambda: {'plus': IntervalTree(), 'minus': IntervalTree()})
 
 for i in range(len(seqids)):
-    seqid = seqids[i]
+    seqid = seqids[i].decode('utf8')
     interval = start_ends[i]
     if interval[0] <= interval[1]:
         # plus strand
-        d[seqid]['plus'][interval[0]:interval[1]] = i
+        intervals[seqid]['plus'][interval[0]:interval[1]] = i
     else:
         # minus strand
-        d[seqid]['minus'][interval[1] + 1:interval[0] + 1] = i
+        intervals[seqid]['minus'][interval[1] + 1:interval[0] + 1] = i
 
+gene_borders = dict()
 with sqlite3.connect(args.db_path) as con:
     cur = con.cursor()
     # query all gene intervals with their relevant information
@@ -62,12 +67,25 @@ with sqlite3.connect(args.db_path) as con:
                    'count(distinct(transcript.id)) ' + query_base)
 
     cur.execute(query_plus)
-    gene_borders_plus = cur.fetchall()
+    gene_borders['plus'] = cur.fetchall()
     cur.execute(query_minus)
-    gene_borders_minus = cur.fetchall()
+    gene_borders['minus'] = cur.fetchall()
 
-
-
-
-
-
+with open(f'{args.output_file}.csv', 'w') as f:
+    for strand in ['plus', 'minus']:
+        for (seqid, start, end, n_transcripts) in gene_borders[strand]:
+            # get the indexes of the sequences in the dataset that overlap with this gene
+            seq_idxs = [i.data for i in list(intervals[seqid][strand][start:end])]
+            seq_idxs.sort() # indexes have to be sorted for h5 query
+            if seq_idxs:
+                # concat all the relevant data with it and cut to limits of the gene
+                y_true_section = np.concatenate(y_true[seq_idxs])[start:end]
+                y_pred_section = np.concatenate(y_pred[seq_idxs])[start:end]
+                sw_section = np.concatenate(sw[seq_idxs])[start:end]
+                if np.any(sw_section):
+                    # run through cm to get the genic f1
+                    cm = ConfusionMatrix(None)
+                    cm._add_to_cm(y_true_section, y_pred_section, sw_section)
+                    genic_f1 = cm._print_results()
+                    # writout results
+                    print(','.join([seqid, str(n_transcripts), str(genic_f1)]), file=f)
