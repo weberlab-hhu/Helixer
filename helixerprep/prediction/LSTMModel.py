e71174382402e4f97a99bb703a0d838f11951d7d
Felix Stiehler <felix.stiehler87@gmail.com>
2019-11-28 16:31:56 +0100

    meta losses added to LSTMModel wip

--- helixerprep/prediction/LSTMModel.py
+++ helixerprep/prediction/LSTMModel.py
@@ -7,8 +7,9 @@ import numpy as np
 import tensorflow as tf
 
 from keras_layer_normalization import LayerNormalization
-from keras.models import Sequential
-from keras.layers import LSTM, CuDNNLSTM, Dense, Bidirectional, Activation, Reshape, Dropout
+from keras.models import Sequential, Model
+from keras.layers import (Conv1D, LSTM, CuDNNLSTM, Dense, Bidirectional, Dropout, Reshape, Activation,
+                          concatenate, Input)
 from HelixerModel import HelixerModel, HelixerSequence
 
 
@@ -62,6 +63,7 @@ class LSTMSequence(HelixerSequence):
                 cw = np.sum(cw_arrays, axis=2)
                 # multiply with previous sample weights
                 sw = np.multiply(sw, cw)
+
             if self.transition_weights is not None:
                 sw_t = [np.any((transitions[:, :, :, col] == 1),axis=2) for col in range(6)]
                 sw_t = np.stack(sw_t, axis=2).astype(np.int8)
@@ -73,13 +75,14 @@ class LSTMSequence(HelixerSequence):
                 sw = np.multiply(sw_t, sw)
 
             if self.meta_losses:
-                gc = np.stack(self.gc_contents[usable_idx_slice])
+                gc = np.stack(self.gc_contents[self.usable_idx_slice])
                 gc = np.repeat(gc[:, None], y.shape[1], axis=1)  # repeat for every time step
-                lengths = np.stack(self.coord_lengths[usable_idx_slice])
+                lengths = np.stack(self.coord_lengths[self.usable_idx_slice])
                 lengths = np.repeat(lengths[:, None], y.shape[1], axis=1)
                 meta = np.stack([gc, lengths], axis=2)
                 y = [y, meta]
                 sw = [sw, sw]
+
         return X, y, sw
 
 
@@ -98,33 +101,48 @@ class LSTMModel(HelixerModel):
         return LSTMSequence
 
     def model(self):
-        model = Sequential()
-
-        model.add(Bidirectional(
-            CuDNNLSTM(self.units, return_sequences=True, input_shape=(None, self.pool_size * 4)),
-            input_shape=(None, self.pool_size * 4)
-        ))
+        main_input = Input(shape=(None, self.pool_size * 4), dtype=self.float_precision,
+                           name='main_input')
+        x = Bidirectional(LSTM(self.units, return_sequences=True))(main_input)
 
         # potential next layers
         if self.layers > 1:
             for _ in range(self.layers - 1):
+                if self.dropout > 0.0:
+                    x = Dropout(self.dropout)(x)
                 if self.layer_normalization:
-                    model.add(LayerNormalization())
-                model.add(Bidirectional(CuDNNLSTM(self.units, return_sequences=True)))
+                    x = LayerNormalization()(x)
+                x = Bidirectional(CuDNNLSTM(self.units, return_sequences=True))(x)
 
         if self.dropout > 0.0:
-            model.add(Dropout(self.dropout))
-        model.add(Dense(self.pool_size * 4))
+            x = Dropout(self.dropout)(x)
+
+        if self.meta_losses:
+            meta_output = Dense(2, activation='sigmoid', name='meta')(x)
+
+        x = Dense(self.pool_size * 4)(x)
         if self.pool_size > 1:
-            model.add(Reshape((-1, self.pool_size, 4)))
-        model.add(Activation('softmax'))
+            x = Reshape((-1, self.pool_size, 4))(x)
+        x = Activation('softmax', name='main')(x)
+
+        outputs = [x, meta_output] if self.meta_losses else [x]
+        model = Model(inputs=main_input, outputs=outputs)
         return model
 
     def compile_model(self, model):
+        if self.meta_losses:
+            meta_loss_weight = 5.0
+            losses = ['categorical_crossentropy', 'mean_squared_error']
+            loss_weights = [1.0, meta_loss_weight]
+        else:
+            losses = ['categorical_crossentropy']
+            loss_weights = [1.0]
+
         run_options = tf.RunOptions(report_tensor_allocations_upon_oom=True)
         run_metadata = tf.RunMetadata()
         model.compile(optimizer=self.optimizer,
-                      loss='categorical_crossentropy',
+                      loss=losses,
+                      loss_weights=loss_weights,
                       sample_weight_mode='temporal',
                       options=run_options,
                       run_metadata=run_metadata)
