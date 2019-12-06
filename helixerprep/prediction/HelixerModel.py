c5a3662296c56e56abb0b6a839db22563b5cf1af
Felix Stiehler <felix.stiehler87@gmail.com>
2019-12-06 13:44:38 +0100

    overlapping fix wip

--- helixerprep/prediction/HelixerModel.py
+++ helixerprep/prediction/HelixerModel.py
@@ -104,6 +104,8 @@ class HelixerSequence(Sequence):
     def _get_batch_data(self, idx):
         usable_idx_batch = self._usable_idx_batch(idx)
         if self.overlap:
+            # overlap continuous sequences that come from the same seqid
+            seqids = self.seqids_dset[usable_idx_batch]
             X = self.x_dset[usable_idx_batch]
             X = np.concatenate(X, axis=0)
             # apply sliding window
@@ -369,11 +371,12 @@ class HelixerModel(ABC):
         chunk_size = predictions.shape[1]
         seqids = test_sequence._get_seqids_for_batch(batch_idx)
         n_cross_seq_preds = chunk_size // self.overlap_offset - 1
-        for i in range(len(seqids) - 1):
-            if seqids[i] != seqids[i + 1]:
-                start_idx = i * (n_cross_seq_preds + 1) + 1
-                zeros = np.zeros((n_cross_seq_preds,) + predictions.shape[1:], dtype=predictions.dtype)
-                predictions[start_idx:start_idx + n_cross_seq_preds] = zeros
+        # for i in range(len(seqids) - 1):
+            # if seqids[i] != seqids[i + 1]:
+                # import pudb; pudb.set_trace()
+                # start_idx = i * (n_cross_seq_preds + 1) + 1
+                # zeros = np.zeros((n_cross_seq_preds,) + predictions.shape[1:], dtype=predictions.dtype)
+                # predictions[start_idx:start_idx + n_cross_seq_preds] = zeros
 
         # actual overlapping; save first and last sequence for special handling later
         first, last = predictions[0], predictions[-1]
