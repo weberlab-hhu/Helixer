0ced72f538d4fc9b1de3562dcf011cdc5c073e60
Felix Stiehler <felix.stiehler87@gmail.com>
2020-01-10 16:23:17 +0100

    minor fixes

--- scripts/alternative_splicing_gene_lengths_errors.py
+++ scripts/alternative_splicing_gene_lengths_errors.py
@@ -156,5 +156,5 @@ with open(f'{args.output_file}', 'w') as f:
                                     str(scores['utr']['f1']),
                                     str(scores['intron']['f1']),
                                     str(scores['exon']['f1']),
-                                    str(scores['sub_genic']['f1'])]), file=f)
+                                    str(scores['sub_genic']['f1']),
                                     str(scores['genic']['f1'])]), file=f)
