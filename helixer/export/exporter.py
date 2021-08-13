85b9ebb682314e3e3474244404c04fa7a05f7706
alisandra <alisandra.denton@hhu.de>
2021-08-13 10:16:06 +0200

    fix typo

--- helixer/export/exporter.py
+++ helixer/export/exporter.py
@@ -166,7 +166,7 @@ class HelixerExportController(HelixerExportControllerBase):
 
     def _coord_info(self, coords_features):
         coord_info = {}
-        for coord_id, coord_len in coord_features.keys():
+        for coord_id, coord_len in coords_features.keys():
             coord = self.exporter.get_coord_by_id(coord_id)
             seqid = coord.seqid.encode('ASCII')
             coord_info[seqid] = (coord_id, coord_len)
