b9c77689e25e3404213f55c78987ad9ec3207fb1
bubu227 <kiel.niklas@live.de>
2021-09-09 16:02:10 +0200

    added f1 score

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -7,12 +7,13 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import functools
-
+import tensorflow_addons as tfa
 
 
 ###################### REFERENCE FILE #######################
 fpath = '/mnt/data/niklas/with_coverage/Mesculenta/test_data.h5'
 
+till = 10000
 
 #class for h5 file containing all methods for analysis
 class H6FILE:
@@ -23,8 +24,8 @@ class H6FILE:
         self.pred_CDS, self.pred_phase = self.get_pred_data()
 
         self.ref = h5py.File(fpath, mode='r')
-        self.ref_CDS = self.ref["data/y"][0:100]
-        self.ref_phase = self.ref["data/phases"][0:100]
+        self.ref_CDS = self.ref["data/y"][0:till]
+        self.ref_phase = self.ref["data/phases"][0:till]
 
     def get_mode(self):
         if len(self.file.keys()) == 2:
@@ -34,9 +35,9 @@ class H6FILE:
 
     def get_pred_data(self):
         if self.is_helixer:
-            return self.file["predictions"][0:100], self.file["predictions_phase"][0:100]
+            return self.file["predictions"][0:till], self.file["predictions_phase"][0:till]
         else:
-            return self.file["data/y"][0:100], self.file["data/phases"][0:100]        
+:c
 
     @staticmethod
     def pred_to_argmax(pred):
@@ -77,9 +78,42 @@ class H6FILE:
             arg_ent= np.array([0, 0, 0])
         return np.array([h_ent, arg_ent]).reshape((-1, 3))
 
+
+    def f1_per_chunk(self, ref_CDS, pred_CDS, ref_phase, pred_phase, argmax_=False):
+        idx = np.sum(ref_CDS, axis=1) != 0
+        ref_CDS = ref_CDS[idx]
+        pred_CDS = pred_CDS[idx].astype(np.float16)
+    
+        ref_phase = ref_phase[idx]
+        pred_phase = pred_phase[idx].astype(np.float16)
+    
+        if argmax_:
+            pred_CDS = self.pred_to_argmax(pred_CDS)
+            pred_phase = self.pred_to_argmax(pred_phase)
+    
+        metric = tfa.metrics.F1Score(num_classes=4)
+        metric.update_state(ref_CDS, pred_CDS)
+        result_cds = metric.result()
+        result_cds.numpy().astype(np.float16)
+    
+        metric = tfa.metrics.F1Score(num_classes=4)
+        metric.update_state(ref_phase, pred_phase)
+        result_phase = metric.result()
+        result_phase.numpy().astype(np.float16)
+    
+        return np.array([result_cds, result_phase]).reshape(-1)
+
+    def f1_by_class(self):
+        h_f1 = np.array(list(map(self.f1_per_chunk, self.ref_CDS, self.pred_CDS, self.ref_phase, self.pred_phase)))
+        cols = range(h_f1.shape[1])
+        means = np.array([np.mean(h_f1[h_f1[:,e] != 0, e]) for e in cols])
+        return np.around(means, decimals=3)
+
     def to_dataframe(self):
         ent = self.entropy()
-        d = {"entropy": ent}
+        f1_score = self.f1_by_class()
+        
+        d = {"entropy": ent, 'f1': f1_score}
         df = pd.DataFrame(data=list(d.items()))
         return df 
 
@@ -95,7 +129,9 @@ def run():
             print(file)
             data_class = H6FILE(file)
             df = data_class.to_dataframe()
-            print(df)
+            print(df.to_string())
+            data_class.file.close()
+            data_class.ref.close()
         print("________________________")    
 
 
