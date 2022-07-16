02e5e949c510e581de22b1f1b4503cf2d36d04b0
alisandra <alisandra.denton@hhu.de>
2022-07-16 10:01:38 +0200

    clean up parameter usage and deaults

--- geenuff2h5.py
+++ geenuff2h5.py
@@ -16,10 +16,10 @@ def main(args):
         match_existing = False
         h5_group = '/data/'
 
-    write_by = round(args.write_by / args.chunk_size) * args.chunk_size
+    write_by = round(args.write_by / args.subsequence_length) * args.subsequence_length
     controller = HelixerExportController(args.input_db_path, args.h5_output_path, match_existing=match_existing,
                                          h5_group=h5_group)
-    controller.export(chunk_size=args.chunk_size, write_by=write_by, modes=modes, compression=args.compression,
+    controller.export(chunk_size=args.subsequence_length, write_by=write_by, modes=modes, compression=args.compression,
                       multiprocess=not args.no_multiprocess)
 
 
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                             help='Outputs the datasets under alternatives/{add-additional}/ (and checks sort order against '
                                  'existing "data" datasets). Use to add e.g. additional annotations from Augustus.')
     pp.data_group.add_argument('--subsequence-length', type=int,
-                              help='Length of the subsequences that the model will use at once. (Default is 20000)')
+                              help='Length of the subsequences that the model will use at once. (Default is 21384)')
     pp.data_group.add_argument('--modes', type=str,
                               help='Either "all" (default), or a comma separated list with desired members of the following '
                                    '{X, y, anno_meta, transitions} that should be exported. This can be useful, for '
@@ -39,11 +39,11 @@ if __name__ == '__main__':
                                    'you are adding an additional annotation set to an existing file.')
     pp.data_group.add_argument('--write-by', type=int,
                               help='Write in super-chunks with this many base pairs, which will be rounded to be '
-                                   'divisible by subsequence-length. (Default is 10_000_000_000).')
+                                   'divisible by subsequence-length. (Default is 21_384_000_000).')
 
     # need to add any default values like this
     pp.defaults['add_additional'] = ''
-    pp.defaults['chunk_size'] = 21384
+    pp.defaults['subsequence_length'] = 21384
     pp.defaults['modes'] = 'all'
     pp.defaults['write_by'] = 21_384_000_000
 
