3480d9133f58f0406f02861e3a012ea82ab8aef6
alisandra <alisandra.denton@hhu.de>
2022-12-22 11:24:00 +0100

    add phase to post-hoc cm & f1 calculations

--- scripts/accs_genic_intergenic.py
+++ scripts/accs_genic_intergenic.py
@@ -3,52 +3,77 @@ import h5py
 import numpy as np
 import argparse
 from helixer.prediction.Metrics import ConfusionMatrix as ConfusionMatrix
+import re
+import os
+
+class H5Holder:
+    def __init__(self, h5_data, h5_pred, h5_prediction_dataset):
+        # find phase dataset based on prediction dataset
+        if h5_prediction_dataset in ['predictions', '/predictions']:
+            h5_phase_dataset = "predictions_phase"
+        elif h5_prediction_dataset.endswith('/y'):
+            h5_phase_dataset = re.sub('/y$', '/phases', args.h5_prediction_dataset)
+        else:
+            raise ValueError(f"do not know how to find phase dataset from {args.h5_prediction_dataset}")
+
+        # get comparable subset of data
+        # category
+        self.data_y = h5_data['data/y']
+        self.pred_y = h5_pred[h5_prediction_dataset]
+        # phase
+        self.data_phase = h5_data['data/phases']
+        self.pred_phase = h5_pred[h5_phase_dataset]
+        # sample weights (to mask both of the above)
+        self.sample_weights = h5_data['data/sample_weights']
 
 
 def main(args):
+
     h5_data = h5py.File(args.data, 'r')
     h5_pred = h5py.File(args.predictions, 'r')
 
+    h5h = H5Holder(h5_data, h5_pred, args.h5_prediction_dataset)
+
     # and score
     cm_calc = ConfusionMatrix(None)
+    cm_phase = ConfusionMatrix(['no_phase', 'phase_0', 'phase_1', 'phase_2'])
 
     # sanity check
-    assert h5_data['data/y'].shape[0] == h5_pred[args.h5_prediction_dataset].shape[0]
-
-    # get comparable subset of data
-    h5_data_y = h5_data['data/y']
-    h5_pred_y = h5_pred[args.h5_prediction_dataset]
-    h5_sample_weights = h5_data['data/sample_weights']
+    assert h5h.data_y.shape == h5h.pred_y.shape
 
     # truncate (for devel efficiency, when we don't need the whole answer)
     if args.truncate is not None:
-        h5_data_y = h5_data_y[:args.truncate]
-        h5_pred_y = h5_pred_y[:args.truncate]
-        h5_sample_weights = h5_sample_weights[:args.truncate]
+        for key, val in h5h.__dict__.items():
+            h5h.__setattr__(key, val[:args.truncate])
 
     # random subset (for devel efficiency, or just if we don't care that much about the full accuracy
     if args.sample is not None:
         a_sample = np.random.choice(
-            np.arange(h5_data_y.shape[0]),
+            np.arange(h5h.data_y.shape[0]),
             size=[args.sample],
             replace=False
         )
         a_sample = np.sort(a_sample)
-        h5_data_y = h5_data_y[a_sample]
-        h5_pred_y = h5_pred_y[a_sample]
-        h5_sample_weights = h5_sample_weights[a_sample]
+        for key, val in h5h.__dict__.items():
+            h5h.__setattr__(key, val[a_sample])
 
     # break into chunks (keep mem usage minimal)
     i = 0
     size = 1000
-    while i < h5_data_y.shape[0]:
-        cm_calc.count_and_calculate_one_batch(h5_data_y[i:(i + size)],
-                                              h5_pred_y[i:(i + size)],
-                                              h5_sample_weights[i:(i + size)])
+    while i < h5h.data_y.shape[0]:
+        cm_calc.count_and_calculate_one_batch(h5h.data_y[i:(i + size)],
+                                              h5h.pred_y[i:(i + size)],
+                                              h5h.sample_weights[i:(i + size)])
+        cm_phase.count_and_calculate_one_batch(h5h.data_phase[i:(i + size)],
+                                               h5h.pred_phase[i:(i + size)],
+                                               h5h.sample_weights[i:(i + size)])
         i += size
 
     cm_calc.print_cm()
-    cm_calc.export_to_csvs(args.stats_dir)
+    cm_calc.export_to_csvs(os.path.join(args.stats_dir, 'genic_class'))
+
+    cm_phase.print_cm()
+    cm_phase.export_to_csvs(os.path.join(args.stats_dir, 'phase'))
 
 
 if __name__ == "__main__":
@@ -62,7 +87,6 @@ if __name__ == "__main__":
                              "the other likely option is '/data/y'")
     parser.add_argument('--sample', type=int, default=None,
                         help="take a random sample of the data of this many chunks per sequence")
-    parser.add_argument('--label_dim', type=int, default=4, help="number of classes, 4 (default) or 7")
     parser.add_argument('--stats_dir', type=str,
                         help="export several csv files of the calculated stats in this directory")
     args = parser.parse_args()
