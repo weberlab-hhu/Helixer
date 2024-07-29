eff84c68d25b9e8a240a557f2b86df13540a60dc
Anthony Aylward <aaylward@salk.edu>
2024-07-28 21:49:08 -0700

    move script files from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 from setuptools import setup
 
-setup(
-   scripts=["Helixer.py", "fasta2h5.py", "geenuff2h5.py", "helixer/prediction/HybridModel.py", "scripts/fetch_helixer_models.py"],
-   zip_safe=False,
-)
+setup(zip_safe=False)
